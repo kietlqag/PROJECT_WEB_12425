e730db24b89261f8ce61f2c64febe7b1e1c1b07b
Le Quoc Kiet <kietlq@gmail.com>
2024-11-14 00:39:07 +0700

    Update 14/11 3

--- src/main/java/Controllers/Admin/LoginController.java
+++ src/main/java/Controllers/Admin/LoginController.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/Admin-trang-chu")
+@WebServlet("/Admin/Trang-chu")
 public class LoginController extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
