00c6bc88fb718fabcd1916c9a5a036b792d0070a
DinhLoi2104 <dinhloinguyen2010@gmail.com>
2024-11-13 23:00:03 +0700

    lgcontrollervendor1

--- src/main/java/Controllers/Vendor/LoginController.java
+++ src/main/java/Controllers/Vendor/LoginController.java
@@ -1,5 +1,20 @@
 package Controllers.Vendor;
 
-public class LoginController {
+import java.io.IOException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/Vendor-trang-chu")
+public class LoginController extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/Views/Vendor/Login.jsp").forward(req, resp);
+	}
 }
